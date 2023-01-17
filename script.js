9c0f5155bdb7084e61a3b6c7ed4d07b213c47f30
sk sabiruddin <120248500+sekhsabiruddin@users.noreply.github.com>
2023-01-17 12:10:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+  let evaluatedText = document.getElementById("evaluatedText");
+    let letterCount = document.getElementById("letterCount");
+
+    evaluatedText.addEventListener("input", function () {
+        let len = evaluatedText.value.length;
+        letterCount.innerHTML = len;
+
+    })
